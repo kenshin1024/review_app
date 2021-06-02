c6f01c2f2d351dca8ac96565a40f1957631c57ff
kenshin1024 <imanishi555@icloud.com>
2021-06-02 21:16:37 +0900

    アソシエーションの記述

--- db/migrate/20210601143239_create_artists.rb
+++ db/migrate/20210601143239_create_artists.rb
@@ -2,6 +2,7 @@ class CreateArtists < ActiveRecord::Migration[6.0]
   def change
     create_table :artists do |t|
       t.string :title, null: false
+      t.references :user, foreign_key: true
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_01_143239) do
+ActiveRecord::Schema.define(version: 2021_06_02_085250) do
+
+  create_table "albums", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "title", null: false
+    t.bigint "user_id"
+    t.bigint "artist_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["artist_id"], name: "index_albums_on_artist_id"
+    t.index ["user_id"], name: "index_albums_on_user_id"
+  end
 
   create_table "artists", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "title", null: false
+    t.bigint "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_artists_on_user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -31,4 +43,7 @@ ActiveRecord::Schema.define(version: 2021_06_01_143239) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "albums", "artists"
+  add_foreign_key "albums", "users"
+  add_foreign_key "artists", "users"
 end
